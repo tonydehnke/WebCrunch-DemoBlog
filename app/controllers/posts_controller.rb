4a5fd7de0faf5e999b78744e45e3e688d49e4393
Tony Dehnke <tdehnke@me.com>
2019-01-07 19:05:25 +0700

    added edit and delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,61 @@
 class PostsController < ApplicationController
 
   def index
+    @posts = Post.all.order("created_at DESC")
   end
 
-  def def new
-    @ = .new
+  def new
+    @post = Post.new
   end
 
-  def def create
-    @object = Object.new(params[:object])
-    if @object.save
+  def create
+    @post = Post.new(post_params)
+    if @post.save
       flash[:success] = "Object successfully created"
-      redirect_to @object
+      redirect_to @post
     else
       flash[:error] = "Something went wrong"
       render 'new'
     end
   end
   
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+      if @post.update(post_params)
+        flash[:success] = "Post was successfully updated"
+        redirect_to @post
+      else
+        flash[:error] = "Something went wrong"
+        render 'edit'
+      end
+  end
   
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    if @post.destroy
+      flash[:success] = 'Post was successfully deleted.'
+      redirect_to posts_path
+    else
+      flash[:error] = 'Something went wrong'
+      redirect_to posts_path
+    end
+  end
+  
+  
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
 
 end
